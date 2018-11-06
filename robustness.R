1340822691c23dc5c7aa3dac9dc6f320b4c99733
Matt Dahl <matt.dahl.2013@gmail.com>
2018-11-05 19:57:11 -0500

    Tidies robustness script.

--- robustness.R
+++ robustness.R
@@ -12,6 +12,8 @@ library(scales)
 #	Robustness checking script
 #############################################
 
+### DATA PREPARATION
+
 ## Define case types
 caseTypes <- c('Abortion')
 caseTypesLabels <- c('Abortion')
@@ -61,23 +63,16 @@ for (i in 1:case_ids.length) {
   Names[i] <- case_data$caseName[case_data$CaseID == case_ids[i]]
 }
 
-AllYears <- c(AllYears,Year)
-AllAuthority <- c(AllAuthority,Authority)
-AllCentrality <- c(AllCentrality,Centrality)
-AllOxford <- c(AllOxford,Oxford)
-AllLii <- c(AllLii,Lii)
-AllNyt <- c(AllNyt,Nyt)
-AllHub <- c(AllHub,Hub)	
-AllNames <- c(AllNames,Names)
+AllYears <- c(AllYears, Year)
+AllAuthority <- c(AllAuthority, Authority)
+AllCentrality <- c(AllCentrality, Centrality)
+AllOxford <- c(AllOxford, Oxford)
+AllLii <- c(AllLii, Lii)
+AllNyt <- c(AllNyt, Nyt)
+AllHub <- c(AllHub, Hub)	
+AllNames <- c(AllNames, Names)
 
-
-all.indicators <- cbind(allChildren,AllYears,AllAuthority,AllCentrality,AllOxford,AllLii,AllNyt,AllHub,AllNames)
-colnames(all.indicators) <- c('allChildren','AllYears','AllAuthority','AllCentrality','AllOxford','AllLii','AllNyt','AllHub','AllNames')
-clean.all.indicators <- all.indicators[is.na(allChildren)==FALSE,]
-for (j in 2:9) {
-  clean.all.indicators <- clean.all.indicators[is.na(clean.all.indicators[,j])==FALSE,]
-}
-
+## Calculate the total number of immediate children for each case
 Xi <- rowMeans(Xi.chain, dims = 2)
 ParentId <- apply(Xi, 1, which.max)
 parentMatrix <- matrix(0, nrow = dim(Xi)[1], ncol = dim(Xi)[1])
@@ -88,11 +83,12 @@ childCases <- colSums(parentMatrix)
 allChildren <- c(allChildren, childCases)
 
 
+### FIGURE CREATION
+
 ## Figure comparing actual and predicted citation counts
 (function () {
-  
-  pdf('figures/Robustness/PredictedCitations.pdf',width=15,height=12)
-  par(mfrow=c(4,5))
+  pdf('figures/Robustness/PredictedCitations.pdf', width = 15, height = 12)
+  par(mfrow=c(4, 5))
 
   sims <- dim(Xi.chain)[3]
   T <- dim(Xi.chain)[1]
@@ -122,31 +118,42 @@ allChildren <- c(allChildren, childCases)
   loess.out <- loess(PredictedVsActual[,2]~PredictedVsActual[,1])
   lines(sqrt(loess.out$x),sqrt(loess.out$fitted),lwd=2)
   
-  
-  ##}
   dev.off()
 })()
 
 
 ## Figure with distribution of parent assignment probabilities
 (function () {
-  pdf('figures/Robustness/NewParentProbs.pdf',15,12)
-  par(mfrow=c(4,5))
+  pdf('figures/Robustness/NewParentProbs.pdf', 15, 12)
+  par(mfrow=c(4, 5))
 
-    Xi <- rowMeans(Xi.chain,dims=2)
+  Xi <- rowMeans(Xi.chain,dims=2)
   XiDescendentCountProbs <- apply(Xi,1,max)
-  plot(density(XiDescendentCountProbs),main=caseTypesLabels[i], xlab='Posterior Parent Probability', ylab='Density'
-       ,xlim=c(0,1))
-  abline(v=mean(XiDescendentCountProbs), col='grey')
+  plot(
+    density(XiDescendentCountProbs),
+    main = caseTypesLabels[i],
+    xlab = 'Posterior Parent Probability',
+    ylab = 'Density',
+    xlim = c(0,1)
+  )
+  abline(v = mean(XiDescendentCountProbs), col='grey')
+  
   dev.off()
 })()
 
 ## Figure with distribution of total children
 (function () {
-  pdf('figures/Robustness/DistributionOfTotalChildren.pdf',6,6)
-  barplot(table(allChildren), main='Distribution of Total Number\nof Child Cases (Truncated)',
-          xlab='Number of Estimated Child Cases',ylab='Density',
-          xlim=c(0,14),col='grey')
+  pdf('figures/Robustness/DistributionOfTotalChildren.pdf', 6, 6)
+  
+  barplot(
+    table(allChildren),
+    main = 'Distribution of Total Number\nof Child Cases (Truncated)',
+    xlab = 'Number of Estimated Child Cases',
+    ylab = 'Density',
+    xlim = c(0,14),
+    col = 'grey'
+  )
+  
   dev.off()
 })()
 
@@ -154,10 +161,16 @@ allChildren <- c(allChildren, childCases)
 (function () {
   allAfterChildren <- NULL
   allBeforeChildren <- NULL
-  pdf('figures/Robustness/DistributionOfParentAge.pdf',6,6)
-  plot(c(0,40),c(0,.15), main='Distribution of Parent\nAge at Childbirth',
-       xlab='Age of Estimated Parent Case',ylab='Density',type='n')
-  colors <- c('black',rep('grey',10))
+  pdf('figures/Robustness/DistributionOfParentAge.pdf', 6, 6)
+  plot(
+    c(0, 40),
+    c(0, 0.15),
+    main = 'Distribution of Parent\nAge at Childbirth',
+    xlab = 'Age of Estimated Parent Case',
+    ylab = 'Density',
+    type = 'n'
+  )
+  colors <- c('black', rep('grey', 10))
 
   Xi <- rowMeans(Xi.chain,dims=2)
   ParentId <- apply(Xi,1,which.max)
@@ -176,7 +189,11 @@ allChildren <- c(allChildren, childCases)
     
   }
   ageAtChildbirth <- ifelse(ageAtChildbirth<0,NA,ageAtChildbirth)
-  lines(density(ageAtChildbirth, na.rm=TRUE), col=colors[i])
+  
+  lines(
+    density(ageAtChildbirth, na.rm = TRUE),
+    col = colors[1]
+  )
   
   dev.off()
 })()
@@ -184,7 +201,14 @@ allChildren <- c(allChildren, childCases)
 
 ## Figure that compares estimates of children with expert lists and NY Times list
 (function () {
-  # first, clean up data
+  # First, clean up data
+  all.indicators <- cbind(allChildren,AllYears,AllAuthority,AllCentrality,AllOxford,AllLii,AllNyt,AllHub,AllNames)
+  colnames(all.indicators) <- c('allChildren','AllYears','AllAuthority','AllCentrality','AllOxford','AllLii','AllNyt','AllHub','AllNames')
+  clean.all.indicators <- all.indicators[is.na(allChildren)==FALSE,]
+  for (j in 2:9) {
+    clean.all.indicators <- clean.all.indicators[is.na(clean.all.indicators[,j])==FALSE,]
+  }
+  
   children <- as.numeric(clean.all.indicators[,1])
   lii <- as.numeric(clean.all.indicators[,6])
   ox <- as.numeric(clean.all.indicators[,5])
@@ -204,23 +228,58 @@ allChildren <- c(allChildren, childCases)
   OxfordXs <- seq(0:15)[which(!is.na(OxfordByChildren))]
   NytXs <- seq(0:15)[which(!is.na(NytByChildren))]
   
-  pdf('figures/Robustness/ComparisonWithLists.pdf',12,4)
+  pdf('figures/Robustness/ComparisonWithLists.pdf', 12, 4)
   par(mfrow=c(1,3))
-  plot(LiiXs, LiiByChildren[which(!is.na(LiiByChildren))], 
-       ylab='Legal Information Institute List Inclusion',xlab='Number of Child Cases',
-       main='Legal Information Institute List and Fertility',
-       xlim=c(0,15), ylim=c(0,1), type='b')
-  points(jitter(allChildren),AllLii,pch="|", col='grey')
-  plot(OxfordXs, OxfordByChildren[which(!is.na(OxfordByChildren))],
-       ylab='Oxford Important Case List Inclusion',xlab='Number of Child Cases',
-       main='Oxford List and Fertility',
-       xlim=c(0,15), ylim=c(0,1), type='b')
-  points(jitter(allChildren),AllOxford, pch="|", col='grey')
-  plot(NytXs, NytByChildren[which(!is.na(NytByChildren))],
-       ylab='New York Times Coverage',xlab='Number of Child Cases',
-       main='Salience and Fertility',
-       xlim=c(0,15), ylim=c(0,1), type='b')
-  points(jitter(allChildren),AllNyt, pch="|", col='grey')
+  
+  plot(
+    LiiXs,
+    LiiByChildren[which(!is.na(LiiByChildren))],
+    ylab = 'Legal Information Institute List Inclusion',
+    xlab = 'Number of Child Cases',
+    main = 'Legal Information Institute List and Fertility',
+    xlim = c(0, 15),
+    ylim = c(0, 1),
+    type = 'b'
+  )
+  points(
+    jitter(allChildren),
+    AllLii,
+    pch = '|',
+    col = 'grey'
+  )
+  plot(
+    OxfordXs,
+    OxfordByChildren[which(!is.na(OxfordByChildren))],
+    ylab = 'Oxford Important Case List Inclusion',
+    xlab = 'Number of Child Cases',
+    main = 'Oxford List and Fertility',
+    xlim = c(0, 15),
+    ylim = c(0, 1),
+    type = 'b'
+  )
+  points(
+    jitter(allChildren),
+    AllOxford,
+    pch = '|',
+    col = 'grey'
+  )
+  plot(
+    NytXs,
+    NytByChildren[which(!is.na(NytByChildren))],
+    ylab = 'New York Times Coverage',
+    xlab = 'Number of Child Cases',
+    main = 'Salience and Fertility',
+    xlim = c(0, 15),
+    ylim = c(0, 1),
+    type = 'b'
+  )
+  points(
+    jitter(allChildren),
+    AllNyt,
+    pch = '|',
+    col = 'grey'
+  )
+
   dev.off()
 })()
 
@@ -229,13 +288,31 @@ allChildren <- c(allChildren, childCases)
 (function () {
   pdf('figures/Robustness/ComparisonWithFowler.pdf', 12, 6)
   par(mfrow=c(1,2))
-  plot(jitter(rescale(AllAuthority)),jitter(rescale(allChildren)), pch=21, cex=0.75, col='grey',bg='grey',
-       xlab='Normalized Fowler and Jeon Authority Score',ylab='Normalized Number of Child Cases',
-       main='Legal Authority and Fertility')
+  
+  plot(
+    jitter(rescale(AllAuthority)),
+    jitter(rescale(allChildren)),
+    pch = 21,
+    cex = 0.75,
+    col = 'grey',
+    bg = 'grey',
+    xlab = 'Normalized Fowler and Jeon Authority Score',
+    ylab = 'Normalized Number of Child Cases',
+    main = 'Legal Authority and Fertility'
+  )
   abline(lm(rescale(allChildren)~rescale(AllAuthority)))
-  plot(jitter(rescale(AllHub)),jitter(rescale(allChildren)), pch=21, cex=0.75, col='grey',bg='grey',
-       xlab='Normalized Fowler and Jeon Hub Score',ylab='Normalized Number of Child Cases',
-       main='Legal Grounding and Fertility')
+  plot(
+    jitter(rescale(AllHub)),
+    jitter(rescale(allChildren)),
+    pch = 21,
+    cex = 0.75,
+    col = 'grey',
+    bg = 'grey',
+    xlab = 'Normalized Fowler and Jeon Hub Score',
+    ylab = 'Normalized Number of Child Cases',
+    main = 'Legal Grounding and Fertility'
+  )
   abline(lm(rescale(allChildren)~rescale(AllHub)))
+  
   dev.off()
 })()
